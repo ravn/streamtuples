fb7180581b9db510f46c39bfa6c957d6127f7b30
ravn <thorbjoern@gmail.com>
2018-12-26 22:56:00 +0100

    Rewrote database test and updated all Maven dependencies.

--- integration-test/src/test/java/dk/ravnand/stream/tests/DatabaseTest.java
+++ integration-test/src/test/java/dk/ravnand/stream/tests/DatabaseTest.java
@@ -1,26 +1,31 @@
 package dk.ravnand.stream.tests;
 
 import dk.ravnand.stream.StreamTuple;
+import io.vavr.control.Try;
+import org.apache.commons.dbutils.ResultSetIterator;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.toMap;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @noinspection WeakerAccess, SqlNoDataSourceInspection
+ * @noinspection WeakerAccess, SqlNoDataSourceInspection, Convert2MethodRef
  */
 public class DatabaseTest {
 
@@ -41,55 +46,77 @@ public class DatabaseTest {
     }
 
     @Test
-    public void pingDatabase() throws SQLException {
-        try (var conn = Objects.requireNonNull(datasource, "datasource").getConnection()) {
-            var statement = conn.prepareStatement("SELECT 1");
-            assertTrue(statement.execute(), "execute()");
+    public void isDatabaseWellBehaved() throws SQLException {
+        try (var conn = Objects.requireNonNull(datasource, "datasource").getConnection();
+             var statement = conn.prepareStatement("SELECT 1 AS ROW1");
+             var resultSet = statement.executeQuery();
+        ) {
+            // Meta data
+            assertEquals(1, resultSet.getMetaData().getColumnCount());
+            assertEquals("ROW1", resultSet.getMetaData().getColumnName(1));
+
+            // Cursor before first row
+            assertTrue(resultSet.isBeforeFirst());
+            assertFalse(resultSet.isLast());
+            assertFalse(resultSet.isAfterLast());
+
+            // First row
+            assertEquals(true, resultSet.next());
+            assertEquals(1, resultSet.getInt(1));
+
+            // Cursor after first row, no more rows.
+            assertFalse(resultSet.isBeforeFirst());
+            assertTrue(resultSet.isLast());
+            assertFalse(resultSet.isAfterLast());
+
+            assertEquals(false, resultSet.next());
+
+            // Cursor after last row.
+            assertFalse(resultSet.isBeforeFirst());
+            assertFalse(resultSet.isLast());
+            assertTrue(resultSet.isAfterLast());
         }
     }
 
-    /**
-     * @noinspection Convert2MethodRef
-     */
-    @Test
+       @Test
     public void streamUpdateSimpleTable() throws SQLException {
         try (var conn = Objects.requireNonNull(datasource, "datasource").getConnection();
-             var s = new WrappedStatement(conn)) {
+             var statement = conn.createStatement()) {
 
             Stream.of("CREATE TABLE t (id INTEGER, s VARCHAR)",
                     "INSERT INTO t VALUES (3, 'Third')",
                     "INSERT INTO t VALUES (2, 'Second')",
                     "INSERT INTO t VALUES (1, 'First')",
                     "COMMIT")
-                    .forEach(s::execute);
+                    .forEach(s -> Try.of(() -> statement.execute(s)).get());
 
             // ---
-            // Ask database for each id.
+            // Ask database for each id.  We use Try.of to tame some of the SQLExceptions.  This is work in progress.
 
             var updateResult = Stream.of(2, 3)
                     .map(StreamTuple::create)
-                    .map(st -> st.map(id -> {
-                        final String sql = "SELECT * FROM t WHERE id=" + id;
-                        try (ResultSet rs = s.executeQuery(sql)) {
-                            rs.next();
-                            return rs.getString("s");
+                    .map(st -> st.map(id -> { // ResultSets do not work well in streams. Better solution pending.
+                        try (var resultSet = statement.executeQuery("SELECT * FROM t WHERE id=" + id)) {
+                            // https://stackoverflow.com/a/24511534/53897
+                            Object[] row = StreamSupport.stream(ResultSetIterator.iterable(resultSet).spliterator(), false)
+                                    .findFirst()
+                                    .get();
+                            return row[1];
                         } catch (SQLException e) {
-                            throw new RuntimeException(sql, e);
+                            throw new RuntimeException(e);
                         }
                     }))
                     .map(st -> st.map(v -> ">" + v + "<"))
-                    .map(st -> st.map((id, v) -> s.execute("UPDATE t SET s='" + v + "' WHERE id=" + id)))
+                    .map(st -> st.map((id, v) -> Try.of(() -> statement.execute("UPDATE t SET s='" + v + "' WHERE id=" + id)).get()))
                     .collect(toMap(st -> st.left(), st -> st.right()));
 
-            var expectedUpdateResult = new HashMap<>();
-            expectedUpdateResult.put(2, false);
-            expectedUpdateResult.put(3, false);
+            var expectedUpdateResult = Map.of(2, false, 3, false);
             assertThat(updateResult, is(expectedUpdateResult));
 
-            // ---  Do we have what we need.
+            // ---  Do we have what we need?
 
             final var result = new HashMap<>();
-            try (ResultSet rs = s.executeQuery("SELECT * from t")) {
+            try (ResultSet rs = statement.executeQuery("SELECT * from t")) {
                 while (rs.next()) {
                     result.put(rs.getString("id"), rs.getString("s"));
                 }
